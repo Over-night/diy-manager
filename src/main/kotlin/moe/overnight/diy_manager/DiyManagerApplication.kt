ab2c4ded64c97f63f947d0c6b5506c15f207907d
Over-Night <hidden98716@gmail.com>
2024-06-21 22:12:00 +0900

    initial settings

---
+++ src/main/kotlin/moe/overnight/diy_manager/DiyManagerApplication.kt
@@ -0,0 +1,11 @@
+package moe.overnight.diy_manager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DiyManagerApplication
+
+fun main(args: Array<String>) {
+	runApplication<DiyManagerApplication>(*args)
+}
---
+++ src/test/kotlin/moe/overnight/diy_manager/DiyManagerApplicationTests.kt
@@ -0,0 +1,13 @@
+package moe.overnight.diy_manager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DiyManagerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
